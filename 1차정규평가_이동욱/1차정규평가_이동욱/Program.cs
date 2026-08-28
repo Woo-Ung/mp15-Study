d6a2dae8fd0a0124cbdd7820a8a99550165e6438
Woo-Ung <ehddnrdlaos@gmail.com>
2026-08-28 10:21:26 +0900

    [Feat] 프로젝트 및 Food 클래스 생성

---
+++ 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    const string FOOD_STORE_NAME = "던전 국밥";
+    static void Main()
+    {
+        
+
+
+    }
+}
+
+public class Food
+{
+    protected string FName;
+    protected string FType;
+    protected int FPrice;
+
+    public Food(string fName, string fType, int fPrince)
+    {
+        FName = fName;
+        FType = fType;
+        FPrice = fPrince;
+    }
+}
