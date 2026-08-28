a6b6b984aca686af8433c97956f171f32f10b820
Woo-Ung <ehddnrdlaos@gmail.com>
2026-08-28 11:25:01 +0900

    [Fix] enum 메뉴, 음식 수정 및 클래스 PrintInfo생성

--- 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
+++ 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
@@ -11,27 +11,42 @@ class Program
             new Rice("용 꼬리 국밥", FoodType.국밥,10000),
             new Stew("얼큰 용암 슬라임 찌개", FoodType.찌개,7000),
             new Stew("복불복 미믹 찌개", FoodType.찌개,8000),
-            new Side("하피 알",FoodType.사이드, 500),
-            new Side("다진 만드라고라",FoodType.사이드, 300),
+            new Side("다진 만드라고라",FoodType.추가, 300),
+            new Side("하피 알",FoodType.추가, 500),            
             new Drink("탄산 슬라임", FoodType.음료, 1000),
             new Drink("세계수 이슬", FoodType.음료, 2000)
-
         };
 
+        List<Food> myMenu = new List<Food>();
+
+        Console.WriteLine("=====================");
+        Console.WriteLine($"===== {FOOD_STORE_NAME} =====");
+        Console.WriteLine("=====================");
+        Console.WriteLine();
+
+        for (int i = 0; i < memu.Length; i++)
+        {
+            memu[i].PrintInfo();
+        }
+
+        Console.WriteLine("1. 주문  2. 전체 비우기  3. 결제  4.영업 종료");
+
+        Console.WriteLine("***메뉴를 선택해주세요***");
+        Console.WriteLine("1. 메인  2. 사이드");
 
 
 
     }
-    public void SelectMenuType(int mType)
+    public void SelectMenuType(MenuType mType)
     {
         switch (mType)
         {
-            case 0:
+            case MenuType.메인:
                 Console.WriteLine(FoodType.국밥);
                 Console.WriteLine(FoodType.찌개);
                 break;
-            case 1:
-                Console.WriteLine(FoodType.사이드);
+            case MenuType.사이드:
+                Console.WriteLine(FoodType.추가);
                 Console.WriteLine(FoodType.음료);
                 break;
             default:
@@ -43,15 +58,15 @@ class Program
 
 public enum MenuType
 {
-    메인,
-    추가
+    메인 = 1,
+    사이드
 }
 
 public enum FoodType
 {
-    국밥,
+    국밥 = 1,
     찌개,
-    사이드,
+    추가,
     음료
 }
 
@@ -66,6 +81,10 @@ public abstract class Food
         FName = fName;
         FType = fType;
         FPrice = fPrince;
+    }
+    public void PrintInfo()
+    {
+        Console.WriteLine($"[{FType}] {FName} : {FPrice}원");
     }
 }
 
