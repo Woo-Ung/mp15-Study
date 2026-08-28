d21845f40bee9434402ef0a30e4f4cdd8c4fad20
Woo-Ung <ehddnrdlaos@gmail.com>
2026-08-28 11:10:00 +0900

    [Feat] Food클래스 FType타입 변경 및 음식 메뉴 생성

--- 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
+++ 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 class Program
 {
     const string FOOD_STORE_NAME = "던전 국밥";
     static void Main()
     {
-        Food[] memu = new Food[] { };
+        Food[] memu = new Food[] 
+        {             
+            new Rice("타우 머리 국밥", FoodType.국밥,9000),
+            new Rice("용 꼬리 국밥", FoodType.국밥,10000),
+            new Stew("얼큰 용암 슬라임 찌개", FoodType.찌개,7000),
+            new Stew("복불복 미믹 찌개", FoodType.찌개,8000),
+            new Side("하피 알",FoodType.사이드, 500),
+            new Side("다진 만드라고라",FoodType.사이드, 300),
+            new Drink("탄산 슬라임", FoodType.음료, 1000),
+            new Drink("세계수 이슬", FoodType.음료, 2000)
+
+        };
 
 
 
@@ -15,8 +27,12 @@ class Program
         switch (mType)
         {
             case 0:
+                Console.WriteLine(FoodType.국밥);
+                Console.WriteLine(FoodType.찌개);
                 break;
             case 1:
+                Console.WriteLine(FoodType.사이드);
+                Console.WriteLine(FoodType.음료);
                 break;
             default:
                 break;
@@ -42,10 +58,10 @@ public enum FoodType
 public abstract class Food
 {
     protected string FName;
-    protected string FType;
+    protected FoodType FType;
     protected int FPrice;
 
-    public Food(string fName, string fType, int fPrince)
+    public Food(string fName, FoodType fType, int fPrince)
     {
         FName = fName;
         FType = fType;
@@ -55,7 +71,7 @@ public abstract class Food
 
 public class Rice : Food
 {
-    public Rice(string fName, string fType, int fPrince) : base(fName, fType, fPrince)
+    public Rice(string fName, FoodType fType, int fPrince) : base(fName, fType, fPrince)
     {
 
     }
@@ -63,7 +79,7 @@ public class Rice : Food
 
 public class Stew : Food
 {
-    public Stew(string fName, string fType, int fPrince) : base(fName, fType, fPrince)
+    public Stew(string fName, FoodType fType, int fPrince) : base(fName, fType, fPrince)
     {
 
     }
@@ -71,7 +87,7 @@ public class Stew : Food
 
 public class Side : Food
 {
-    public Side(string fName, string fType, int fPrince) : base(fName, fType, fPrince)
+    public Side(string fName, FoodType fType, int fPrince) : base(fName, fType, fPrince)
     {
 
     }
@@ -79,7 +95,7 @@ public class Side : Food
 
 public class Drink : Food
 {
-    public Drink(string fName, string fType, int fPrince) : base(fName, fType, fPrince)
+    public Drink(string fName, FoodType fType, int fPrince) : base(fName, fType, fPrince)
     {
 
     }
