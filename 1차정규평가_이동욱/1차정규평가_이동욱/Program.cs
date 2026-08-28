76fb06d76619627726e81ab3baf8676cb5fc784c
Woo-Ung <ehddnrdlaos@gmail.com>
2026-08-28 10:34:10 +0900

    [Feat] enum 메뉴, 음식 타입 생성

--- 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
+++ 1차정규평가_이동욱/1차정규평가_이동욱/Program.cs
@@ -4,13 +4,30 @@ class Program
     const string FOOD_STORE_NAME = "던전 국밥";
     static void Main()
     {
-        
+        Food[] memu = new Food[] { };
+
+
 
 
     }
 }
 
-public class Food
+public enum MenuType
+{
+    메인,
+    추가
+}
+
+public enum FoodType
+{
+    국밥,
+    찌개,
+    사이드,
+    음료
+}
+
+
+public abstract class Food
 {
     protected string FName;
     protected string FType;
